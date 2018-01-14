bba238b8bd99da2893beed77ca3397980ada9eeb
VanausloosThomas <thomas.vanausloos@cegeka.com git config --global core.autocrlf inputgit config --global user.email thomas.vanausloos@cegeka.com>
2018-01-14 10:47:16 +0100

    added project files and .gitignore fire

---
+++ list-app/src/main/java/com/vanausloosthomas/listapp/ListAppApplication.java
@@ -0,0 +1,12 @@
+package com.vanausloosthomas.listapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListAppApplication.class, args);
+	}
+}
---
+++ list-app/src/test/java/com/vanausloosthomas/listapp/ListAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vanausloosthomas.listapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ListAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
